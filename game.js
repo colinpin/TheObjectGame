7807bcf1412d39ca1ebcc5b818f3ef1f440ddf05
davenaugler <dv.naugler@gmail.com>
2016-01-28 18:59:28 -0700

    Submit Changes

--- game.js
+++ game.js
@@ -1,18 +1,36 @@
 var player = {
+    //Step 6: Declaring 2 more variables "name" and "hits"
+    name: "Leonidas",
+    hits: 0,
     health: 100,
-    slap: function () {
+    slap: function (slap) {
         this.health--;
+        this.hits ++;
         this.update();
+        this.numOfHits();
+        
     },
-    punch: function () {
+    punch: function (punch) {
         this.health -= 5;
+        this.hits ++;
         this.update();
+        this.numOfHits();
+       
     },
-    kick: function () {
+    kick: function (kick) {
         this.health -= 10;
+        this.hits ++;
         this.update();
+        this.numOfHits();
     },
     update: function () {
         document.getElementById("pHealth").textContent = this.health.toString();
+        
+    },
+    numOfHits: function () {
+        document.getElementById("sHits").textContent = this.hits;
     }
-};
+}
+
+document.getElementById("sName").textContent = player.name;
+document.getElementById("sHits").textContent = player.hits;
